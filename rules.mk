2791cdf16aa2c25f9be00b7b6abac388a7db6bd0
dannoy.lee <dannoy.lee@gmail.com>
2013-11-13 15:48:28 +0800

    add dependency generation in rules.mk

--- rules.mk
+++ rules.mk
@@ -14,6 +14,8 @@ endif
 
 all:
 
+& = $(filter-out %.h %.d %.a,$^)
+
 define compile.prog
 	$(Q) $(CC) -m32 $(LDFLAGS) -o $$@ $$^ -Wl,-Map,$(OBJ)/$(1).map
 endef
@@ -23,11 +25,15 @@ define compile.slib
 endef
 
 define compile.prog.o
-	$(Q) $(CC) $(CFLAGS) -c -o $$@ $$^
+	$(Q) $(CC) $(CFLAGS) -c -o $$@ $(1)
 endef
 
 define compile.slib.o
-	$(Q) $(CC) -fPIC $(CFLAGS) -c -o $$@ $$^
+	$(Q) $(CC) -fPIC $(CFLAGS) -c -o $$@ $(1)
+endef
+
+define compile.d
+	$(Q) $(CC) -MM $(CFLAGS) $$< | sed -n "H;$$$$ {g;s@.*:\(.*\)@$$< := \$$$$\(wildcard\1\)\n$${@:%.d=%.o} $$@: $$$$\($$<\)@;p}" > $$@
 endef
 
 #$(OBJ)/$(1:%.c=%.o):$(1) $(1:%.c=%.h)
@@ -35,35 +41,56 @@ define prog_object_template
 $(filter %.o,$(OBJ)/$(1:%.cpp=%.o) $(OBJ)/$(1:%.c=%.o)):$(1) $(1:%.c=%.h)
 	$(Q) mkdir -p $(@D)
 	$(Q) echo -e "\tCC $$<"
-$(call compile.prog.o, $@)
+$(call compile.prog.o, $$&)
+
+$(filter %.d,$(OBJ)/$(1:%.cpp=%.d) $(OBJ)/$(1:%.c=%.d)):$(1)
+	$(Q) echo -e "\tDEP $$<"
+	@mkdir -p $$(@D)
+$(call compile.d,$(1))
 endef
 
 #$(OBJ)/$(1:%.cpp=%.o):$(1)
 define slib_object_template
-$(filter %.o,$(OBJ)/$(1:%.cpp=%.o) $(OBJ)/$(1:%.c=%.o)):$(1)
+$(filter %.o,$(OBJ)/$(1:%.cpp=%.o) $(OBJ)/$(1:%.c=%.o)):$(1) $(filter %.d,$(OBJ)/$(1:%.cpp=%.d) $(OBJ)/$(1:%.c=%.d))
 	$(Q) mkdir -p $$(@D)
 	$(Q) echo -e "\tSCC $$<"
-$(call compile.slib.o, $@)
+$(call compile.slib.o, $$&)
+
+$(filter %.d,$(OBJ)/$(1:%.cpp=%.d) $(OBJ)/$(1:%.c=%.d)):$(1)
+	$(Q) echo -e "\tSDEP $$<"
+	@mkdir -p $$(@D)
+$(call compile.d,$(1))
 endef
 
 define prog_template
-bin/$(1): $(filter %.o,$($(1)_src:%.cpp=$(OBJ)/%.o) $($(1)_src:%.c=$(OBJ)/%.o))
+bin/$(1): $($(1)_dep) $(filter %.o,$($(1)_src:%.cpp=$(OBJ)/%.o) $($(1)_src:%.c=$(OBJ)/%.o))
 	$(Q) mkdir -p $$(@D)
 	$(Q) echo -e "\tLD $(1)"
 $(call compile.prog,$(1))
+
 $(foreach src,$($(1)_src),$(eval $(call prog_object_template,$(src))))
+
+ifneq ($(MAKECMDGOALS),clean)
+-include $(filter %.d,$($(1)_src:%.cpp=$(OBJ)/%.d) $($(1)_src:%.c=$(OBJ)/%.d))
+endif
+
 progs_target : bin/$(1)
 endef
 
 #lib/$(1).so:$(OBJ)/$($(1)_src:%.c=%.o) $(OBJ)/$($(1)_src:%.cpp=%.o)
 #$(foreach src,$($(1)_src),$(info $(call slib_object_template,$(src))))
 define slib_template
+
 lib/$(1).so: $($(1)_dep) $(filter %.o,$($(1)_src:%.cpp=$(OBJ)/%.o) $($(1)_src:%.c=$(OBJ)/%.o))
 	$(Q) mkdir -p $$(@D)
 	$(Q) echo -e "\tShared lib/$(1).so"
 $(call compile.slib,$(1))
 
 $(foreach src,$($(1)_src),$(eval $(call slib_object_template,$(src))))
+
+ifneq ($(MAKECMDGOALS),clean)
+-include $(filter %.d,$($(1)_src:%.cpp=$(OBJ)/%.d) $($(1)_src:%.c=$(OBJ)/%.d))
+endif
 
 shared_libs_target : lib/$(1).so
 endef
